0f7a33d8e6c15371e7eadb91bcfe28dfb9911bb5
Sarah Creager <sarah.f.jamieson@gmail.com>
2021-10-20 16:22:18 -0700

    refactored middleware and server

--- src/middleware/validator.js
+++ src/middleware/validator.js
@@ -5,6 +5,7 @@ module.exports = function (request, response, next) {
   if (request.query.name) {
     next();
   } else {
+    // this does not "force" a 500 error, but it calls next with a parameter which triggers our 500 error. 
     next('No name');
   }
 };
