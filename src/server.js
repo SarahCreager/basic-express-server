70de52205960baf63e888c9f0a03b2a6b099f2b6
Sarah Creager <sarah.f.jamieson@gmail.com>
2021-10-20 17:34:37 -0700

    fixed testing errors, updated server

--- src/server.js
+++ src/server.js
@@ -6,19 +6,18 @@ const app = express();
 const validator = require('./middleware/validator.js');
 const error404Handler = require('./error-handlers/404.js');
 const error500Handler = require('./error-handlers/500.js');
-const logger = require('./middleware/logger.js')
+const logger = require('./middleware/logger.js');
+
 
 // running logger for all routes in application
 app.use(logger);
 app.use(express.json());
 
 app.get('/person', validator, (request, response)  => {
-  // TODO: response.status(200)
-  let name = request.query.name;
+  let name = request.query;
+  response.status(200);
   response.send(name);
-  // TODO: send back an object
 });
-
 
 app.use(error404Handler);
 app.use(error500Handler);
